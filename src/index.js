817648e6a9dd721ba8fc70a55dc578f18cb0c272
Antonina Muravska <at.tonya@gmail.com>
2020-12-17 11:37:59 +0200

    finished timer

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+// import './js/timer.js';
+import './js/timerByClass.js';
+
---
+++ src/js/timerByClass.js
@@ -0,0 +1,48 @@
+import refs from './refs.js';
+
+let interval;
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+    }
+
+    count(time) {
+       const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+       const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+       const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+       const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        
+       refs.daysRef.textContent = days;
+       refs.hoursRef.textContent = hours;
+       refs.minsRef.textContent = mins;
+       refs.secsRef.textContent = secs;
+
+    }
+
+    timer() {
+        let currentDate = Date.now();
+        const deltaTime = this.targetDate - currentDate;
+        this.count(deltaTime);
+    }
+
+    start() {
+        this.timer();
+        interval = setInterval(() => {
+            this.timer();
+    }, 1000)
+    }
+
+    pad(value) {
+     return String(value).padStart(2, '0');
+  }
+
+}
+
+const myTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 01, 2021'),
+});
+
+myTimer.start();
