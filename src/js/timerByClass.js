30694ce54fa3925965567dc19c883ac6c6e68297
Antonina Muravska <at.tonya@gmail.com>
2020-12-19 22:26:10 +0200

    fix timer

--- src/js/timerByClass.js
+++ src/js/timerByClass.js
@@ -1,24 +1,26 @@
 import refs from './refs.js';
 
-let interval;
+
 
 class CountdownTimer {
     constructor({ selector, targetDate }) {
         this.selector = selector;
         this.targetDate = targetDate;
+        this.intervalId = null;
     }
-
+ 
     count(time) {
        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
         
-       refs.daysRef.textContent = days;
-       refs.hoursRef.textContent = hours;
-       refs.minsRef.textContent = mins;
-       refs.secsRef.textContent = secs;
-
+        if (time > 0) {
+           refs.daysRef.textContent = days;
+           refs.hoursRef.textContent = hours;
+           refs.minsRef.textContent = mins;
+           refs.secsRef.textContent = secs;
+       } else clearInterval(this.intervalId);
     }
 
     timer() {
@@ -26,10 +28,10 @@ class CountdownTimer {
         const deltaTime = this.targetDate - currentDate;
         this.count(deltaTime);
     }
-
+   
     start() {
         this.timer();
-        interval = setInterval(() => {
+        this.intervalId = setInterval(() => {
             this.timer();
     }, 1000)
     }
